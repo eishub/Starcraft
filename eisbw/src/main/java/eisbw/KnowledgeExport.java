33c12b218e7b5bbb7cba8fc6ec4c4ef28830152b
Vincent Koeman <v.j.koeman@tudelft.nl>
2017-07-13 09:06:13 +0200

    fix knowledge exporter for bwmirror

--- eisbw/src/main/java/eisbw/KnowledgeExport.java
+++ eisbw/src/main/java/eisbw/KnowledgeExport.java
@@ -10,6 +10,7 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
+import bwapi.Race;
 import bwapi.TechType;
 import bwapi.TilePosition;
 import bwapi.UnitType;
@@ -24,117 +25,11 @@ public class KnowledgeExport {
 			if (Modifier.isPublic(field.getModifiers()) && Modifier.isStatic(field.getModifiers())) {
 				try {
 					UnitType type = (UnitType) field.get(null);
-					Field pointer = UnitType.class.getDeclaredField("pointer");
-					pointer.setAccessible(true);
-					int id = (int) pointer.get(type);
-					System.out.println(type + " > " + id);
-					switch (id) {
-					// TERRAN UNITS
-					case 0: // marine
-					case 1: // ghost
-					case 2: // vulture
-					case 3: // goliath
-					case 4: // siege tank
-					case 7: // scv
-					case 8: // wraith
-					case 9: // science vessel
-					case 11: // dropship
-					case 12: // battlecruiser
-					case 13: // spider mine (given to vultures)
-					case 32: // firebat
-					case 34: // medic
-					case 58: // valkyrie
-						// ZERG UNITS
-					case 35: // larva
-					case 36: // egg
-					case 37: // zergling
-					case 38: // hydralisk
-					case 39: // ultralisk
-					case 41: // drone
-					case 42: // overlord
-					case 43: // mutalisk
-					case 44: // guardian
-					case 45: // queen
-					case 46: // defiler
-					case 47: // scourge
-					case 59: // cocoon
-					case 62: // devourer
-					case 97: // lurker egg
-					case 103: // lurker
-						// PROTOSS UNITS
-					case 60: // corsair
-					case 61: // dark templar
-					case 63: // dark archon
-					case 64: // probe
-					case 65: // zealot
-					case 66: // dragoon
-					case 67: // high templar
-					case 68: // archon
-					case 69: // shuttle
-					case 70: // scout
-					case 71: // arbiter
-					case 72: // carrier
-					case 73: // interceptor (produced by carriers)
-					case 83: // reaver
-					case 84: // observer
-					case 85: // scarab (produced by reavers)
-						// TERRAN BUILDINGS
-					case 106: // command center
-					case 107: // comsat station
-					case 108: // nuclear silo
-					case 109: // supply depot
-					case 110: // refinery
-					case 111: // barracks
-					case 112: // academy
-					case 113: // factory
-					case 114: // startport
-					case 115: // control tower
-					case 116: // science facility
-					case 117: // covert ops
-					case 118: // physics lab
-					case 120: // machine shop
-					case 122: // engineering bay
-					case 123: // armory
-					case 124: // missile turret
-					case 125: // bunker
-						// ZERG BUILDINGS
-					case 131: // hatchery
-					case 132: // lair
-					case 133: // hive
-					case 134: // nydus canal
-					case 135: // hydralisk den
-					case 136: // defiler mound
-					case 137: // greater spire
-					case 138: // queens nest
-					case 139: // evolution chamber
-					case 140: // ultralisk cavern
-					case 141: // spire
-					case 142: // spawning pool
-					case 143: // creep colony
-					case 144: // spore colony
-					case 146: // sunken colony
-					case 149: // extractor
-						// PROTOSS BUILDINGS
-					case 154: // nexus
-					case 155: // robotics facility
-					case 156: // pylon
-					case 157: // assimilator
-					case 159: // observatory
-					case 160: // gateway
-					case 162: // photon cannon
-					case 163: // citadel of adun
-					case 164: // cybernetics core
-					case 165: // templar archives
-					case 166: // forge
-					case 167: // stargate
-					case 169: // fleet beacon
-					case 170: // arbiter tribunal
-					case 171: // robotics support bay
-					case 172: // shield battery
-						// SPELLS
-					case 33: // scanner sweep
-					case 105: // disruption web
-					case 202: // dark swarm
+					if ((type.getRace() == Race.Terran || type.getRace() == Race.Zerg || type.getRace() == Race.Protoss)
+							&& !(type.isHero() || type.isPowerup() || type.isSpecialBuilding()
+									|| type == UnitType.Terran_Siege_Tank_Siege_Mode
+									|| type == UnitType.Special_Cargo_Ship
+									|| type == UnitType.Special_Mercenary_Gunship)) {
 						export += getUnitType(type);
 						export += getUnitCosts(type);
 						export += getUnitStats(type);
@@ -143,31 +38,42 @@ public class KnowledgeExport {
 							export += getUnitCombat(type);
 						}
 						export += "\n";
-						break;
-					default:
-						break;
 					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		}
-		// for (final TechType type : TechTypes.getAllTechTypes()) {
-		// if ((type.getID() >= 0 && type.getID() <= 25) || (type.getID() >= 27 &&
-		// type.getID() == 46)) {
-		// export += getTechType(type);
-		// export += getTechCosts(type);
-		// export += getTechCombat(type);
-		// export += "\n";
-		// }
-		// }
-		// for (final UpgradeType type : UpgradeTypes.getAllUpgradeTypes()) {
-		// if (type.getID() >= 0 && type.getID() <= 54) {
-		// export += getUpgradeType(type);
-		// export += getUpgradeCosts(type);
-		// export += "\n";
-		// }
-		// }
+		for (Field field : TechType.class.getDeclaredFields()) {
+			if (Modifier.isPublic(field.getModifiers()) && Modifier.isStatic(field.getModifiers())) {
+				try {
+					TechType type = (TechType) field.get(null);
+					if (type != TechType.Unknown && type != TechType.None) {
+						export += getTechType(type);
+						export += getTechCosts(type);
+						export += getTechCombat(type);
+						export += "\n";
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		for (Field field : UpgradeType.class.getDeclaredFields()) {
+			if (Modifier.isPublic(field.getModifiers()) && Modifier.isStatic(field.getModifiers())) {
+				try {
+					UpgradeType type = (UpgradeType) field.get(null);
+					if (type != UpgradeType.Unknown && type != UpgradeType.None && type != UpgradeType.Upgrade_60) {
+						export += getUpgradeType(type);
+						export += getUpgradeCosts(type);
+						export += "\n";
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
 		try {
 			Files.write(Paths.get(new File("export.pl").toURI()), export.getBytes("utf-8"), StandardOpenOption.CREATE,
 					StandardOpenOption.TRUNCATE_EXISTING);
@@ -177,14 +83,14 @@ public class KnowledgeExport {
 	}
 
 	private static String getUnitType(UnitType type) {
-		return String.format("unit('%s',%s).\n", BwapiUtility.getName(type), type.getRace().toString().toLowerCase());
+		return String.format("unit('%s',%s).\n", BwapiUtility.getName(type), getName(type.getRace()));
 	}
 
 	private static String getUnitCosts(UnitType type) {
 		String requirements = "[";
 		boolean hadFirst = false;
 		if (type.requiredTech() != TechType.Unknown && type.requiredTech() != TechType.None) {
-			requirements += "'" + type.requiredTech().toString() + "'";
+			requirements += "'" + getName(type.requiredTech()) + "'";
 			hadFirst = true;
 		}
 		for (UnitType required : type.requiredUnits().keySet()) {
@@ -285,14 +191,14 @@ public class KnowledgeExport {
 	}
 
 	private static String getTechType(TechType type) {
-		return String.format("upgrade('%s',%s).\n", type.toString(), type.getRace().toString().toLowerCase());
+		return String.format("upgrade('%s',%s).\n", getName(type), getName(type.getRace()));
 	}
 
 	private static String getTechCosts(TechType type) {
 		UnitType researches = type.whatResearches();
 		String required = (researches == UnitType.Unknown || researches == UnitType.None) ? ""
 				: ("'" + BwapiUtility.getName(researches) + "'");
-		return String.format("costs('%s',%d,%d,%d,%d,%s).\n", type.toString(), type.mineralPrice(), type.gasPrice(),
+		return String.format("costs('%s',%d,%d,%d,%d,%s).\n", getName(type), type.mineralPrice(), type.gasPrice(),
 				type.energyCost(), type.researchTime(), "[" + required + "]");
 	}
 
@@ -301,7 +207,7 @@ public class KnowledgeExport {
 		if (weapon == WeaponType.Unknown || weapon == WeaponType.None) {
 			return "";
 		} else {
-			return String.format("combat('%s',%d,%d,%d,%d,%d).\n", type.toString(),
+			return String.format("combat('%s',%d,%d,%d,%d,%d).\n", getName(type),
 					weapon.targetsGround() ? (weapon.damageAmount() * weapon.damageFactor()) : 0,
 					weapon.targetsAir() ? (weapon.damageAmount() * weapon.damageFactor()) : 0, weapon.damageCooldown(),
 					weapon.maxRange() / TilePosition.SIZE_IN_PIXELS,
@@ -313,12 +219,11 @@ public class KnowledgeExport {
 		if (type.maxRepeats() > 1) {
 			String returned = "";
 			for (int i = 1; i <= type.maxRepeats(); ++i) {
-				returned += String.format("upgrade('%s',%s).\n", type.toString() + " " + i,
-						type.getRace().toString().toLowerCase());
+				returned += String.format("upgrade('%s',%s).\n", getName(type) + " " + i, getName(type.getRace()));
 			}
 			return returned;
 		} else {
-			return String.format("upgrade('%s',%s).\n", type.toString(), type.getRace().toString().toLowerCase());
+			return String.format("upgrade('%s',%s).\n", getName(type), getName(type.getRace()));
 		}
 	}
 
@@ -329,11 +234,23 @@ public class KnowledgeExport {
 		String returned = "";
 		for (int i = 1; i <= type.maxRepeats(); ++i) {
 			String toAdd = (type.maxRepeats() == 1) ? "" : (" " + i);
-			returned += String.format("costs('%s',%d,%d,%d,%d,%s).\n", type.toString() + toAdd,
+			returned += String.format("costs('%s',%d,%d,%d,%d,%s).\n", getName(type) + toAdd,
 					type.mineralPrice() + (type.mineralPriceFactor() * i),
 					type.gasPrice() + (type.gasPriceFactor() * i), 0,
 					type.upgradeTime() + (type.upgradeTimeFactor() * i), "[" + required + "]");
 		}
 		return returned;
+	}
+
+	private static String getName(Race race) {
+		return race.toString().toLowerCase();
+	}
+
+	private static String getName(TechType type) {
+		return type.toString().replace("_", " ");
+	}
+
+	private static String getName(UpgradeType type) {
+		return type.toString().replace("_", " ");
 	}
 }
