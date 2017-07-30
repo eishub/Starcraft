f004c47d8ed40676608cb761481781643aae0743
Vincent Koeman <v.j.koeman@tudelft.nl>
2017-07-30 13:16:42 +0200

    add an ownRace percept

---
+++ eisbw/src/main/java/eisbw/percepts/OwnRacePercept.java
@@ -0,0 +1,20 @@
+package eisbw.percepts;
+
+import eis.iilang.Identifier;
+import eis.iilang.Percept;
+
+/**
+ * @author Danny & Harm - The Own Race percept.
+ *
+ */
+public class OwnRacePercept extends Percept {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @param name
+	 *            The name of the player's own race.
+	 */
+	public OwnRacePercept(String name) {
+		super(Percepts.OWNRACE, new Identifier(name));
+	}
+}
