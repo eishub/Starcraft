b2066bc99a2b4d320d3dbb74c40ff8ae9bf7a090
Thijs Raymakers <thijs@raymakers.nl>
2018-05-15 13:15:27 +0200

    Add comments to SendText class

--- eisbw/src/main/java/eisbw/actions/SendText.java
+++ eisbw/src/main/java/eisbw/actions/SendText.java
@@ -23,19 +23,37 @@ public class SendText extends StarcraftAction {
         super(api);
     }
 
+    /**
+     * Checks if the invoked sendText action has a syntactically correct parameter.
+	 * @param action The action that is invoked.
+     * @return If the action contains an Identifier as a parameter.
+	 */ 
     @Override
     public boolean isValid(Action action) {
-		return true;
-       // List<Parameter> parameters = action.getParameters();
-     //   return parameters.size() == 1 && parameters.get(0) instanceof Identifier
-    //            && getUnitType(((Identifier) parameters.get(0)).getValue()) != null;
+		List<Parameter> parameters = action.getParameters();
+		return parameters.size() == 1 && parameters.get(0) instanceof Identifier;
     }
 
+    /**
+     * Checks if this unit type can execute this action. Since SendText is not type specific, this should always
+     * return true.
+     * @param type
+     *            The type of the unit performing the action.
+     * @param action
+     *            The evaluated action.
+     * @return true, since sendText is not type specific.
+     */
     @Override
     public boolean canExecute(UnitType type, Action action) {
         return true;
     }
 
+    /**
+     * Execute the sendText command
+     * @param unit
+     *            The unit performing the action.
+     * @param action The action that is being performed.
+     */
     @Override
     public void execute(Unit unit, Action action) {
         List<Parameter> parameters = action.getParameters();
@@ -44,6 +62,9 @@ public class SendText extends StarcraftAction {
         this.api.sendText(text);
     }
 
+    /**
+     * @return The String notation of this method
+     */
     @Override
     public String toString() {
         return "sendText";
